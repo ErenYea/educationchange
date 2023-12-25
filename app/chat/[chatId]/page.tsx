6322777d562e00e7aa1b06ddcf9817b8694700ae
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-12-25 23:12:36 +0500

    Update page.tsx

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -27,6 +27,7 @@ const Chats = (props: Props) => {
   const [userTopics, setUserTopics] = useState<Topic[]>([]);
   const [isLoading, setIsLoading] = useState(false)
   const [loadingChats, setLoadingChats] = useState(true)
+  const [isDeleting, setIsDeleting] = useState(false)
   const [loadingMessages, setLoadingMessages] = useState(true)
   const session = useSession();
   const pathname = usePathname();
@@ -116,11 +117,15 @@ const Chats = (props: Props) => {
   };
 
   const deleteChat = async () => {
-    if (!chatIdToDelete) return;
+    if (!chatIdToDelete || isDeleting) return;
+    setIsDeleting(true)
     await deleteAChat(chatIdToDelete);
+    setIsDeleting(false)
     setShowDeletePopup(false);
     setChatIdToDelete("");
-    getChats();
+    setLoadingChats(true)
+    await getChats();
+    setLoadingChats(false)
   };
 
   return (
@@ -259,8 +264,13 @@ const Chats = (props: Props) => {
                               onClick={deleteChat}
                               className="flex w-full gap-2 items-center justify-center hover:bg-red-500 py-2 px-6 border border-red-400 rounded-md cursor-pointer"
                             >
-                              Delete
-                            </div>
+                              {isDeleting ? 
+                                <div className="flex items-center justify-center space-x-2">
+                                  <p>Deleting...</p>
+                                  <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-white"></div>
+                                </div>
+                                : "Delete"}{" "}
+                                </div>
                             <div
                               onClick={() => setShowDeletePopup(false)}
                               className="flex w-full gap-2 items-center justify-center hover:bg-gray-500 py-2 px-6 border border-gray-400 rounded-md cursor-pointer"
