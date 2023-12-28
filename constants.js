cb0881779fd701ecd82e3c6f4d17af76cb03c284
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-12-29 01:09:11 +0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,91 +1,90 @@
 export const QUADRANT_ONE_INFO = [
-    {
-      name: "Personal Insights",
-      content: [
-        "Daily routines and work habits",
-        "Recommended reading (books, journals, websites)",
-        "Role models or mentors who've influenced his leadership style",
-        "Challenges faced and lessons learned",
-      ],
-    },
-  ];
-  
-  export const QUADRANT_TWO_INFO = [
-    {
-      name: "Professional Background",
-      content: [
-        "Education (degrees, institutions, areas of study)",
-        "Previous roles and work experiences",
-        "Professional achievements and milestones",
-        "Certifications or continued learning experiences",
-      ],
-    },
-    {
-      name: "Leadership Style & Philosophy",
-      content: [
-        "Decision-making processes",
-        "Views on team collaboration and communication",
-        "Core leadership principles and beliefs",
-        "Preferred management styles (e.g., hands-on, delegative)",
-      ],
-    },
-  ];
-  
-  export const QUADRANT_THREE_INFO = [
-    {
-      name: "Vision & Strategy",
-      content: [
-        "The company's mission, vision, and core values",
-        "Long-term and short-term goals for the company",
-        "Views on industry trends and challenges",
-        "Plans for company expansion, diversification, or pivot",
-      ],
-    },
-    {
-      name: "Decision-making Data",
-      content: [
-        "Key performance indicators (KPIs) he frequently monitors",
-        "Crucial reports, metrics, or analytics tools he relies upon",
-        "Common challenges and how he navigates them",
-      ],
-    },
-    {
-      name: "Communication Preferences",
-      content: [
-        "Preferred channels of communication (e.g., email, meetings, video calls)",
-        "Feedback mechanisms",
-        "Communication tone and style (e.g., formal, informal)",
-      ],
-    },
-    {
-      name: "Stakeholder Interactions",
-      content: [
-        "How you engage with different stakeholders (e.g., employees, shareholders, customers)",
-        "Expectations from board meetings, team meetings, etc.",
-      ],
-    },
-    {
-      name: "Industry Insights",
-      content: [
-        "Views on the future of the engineering media industry",
-        "Key competitors and industry benchmarks",
-        "Opportunities and threats in the media landscape",
-      ],
-    },
-    {
-      name: "Crisis Management",
-      content: [
-        "Past experiences managing crises or significant challenges",
-        "Strategies and processes for navigating unforeseen events",
-      ],
-    },
-    {
-      name: "Technology & Innovation",
-      content: [
-        "Views on digital transformation",
-        "Technologies you believe will shape the future",
-        "Innovation strategies",
-      ],
-    },
-  ];
-  
+  {
+    name: "Personal Insights",
+    content: [
+      "Daily routines and work habits",
+      "Recommended reading (books, journals, websites)",
+      "Role models or mentors who've influenced his leadership style",
+      "Challenges faced and lessons learned",
+    ],
+  },
+];
+
+export const QUADRANT_TWO_INFO = [
+  {
+    name: "Professional Background",
+    content: [
+      "Education (degrees, institutions, areas of study)",
+      "Previous roles and work experiences",
+      "Professional achievements and milestones",
+      "Certifications or continued learning experiences",
+    ],
+  },
+  {
+    name: "Leadership Style & Philosophy",
+    content: [
+      "Decision-making processes",
+      "Views on team collaboration and communication",
+      "Core leadership principles and beliefs",
+      "Preferred management styles (e.g., hands-on, delegative)",
+    ],
+  },
+];
+
+export const QUADRANT_THREE_INFO = [
+  {
+    name: "Vision & Strategy",
+    content: [
+      "The company's mission, vision, and core values",
+      "Long-term and short-term goals for the company",
+      "Views on industry trends and challenges",
+      "Plans for company expansion, diversification, or pivot",
+    ],
+  },
+  {
+    name: "Decision-making Data",
+    content: [
+      "Key performance indicators (KPIs) he frequently monitors",
+      "Crucial reports, metrics, or analytics tools he relies upon",
+      "Common challenges and how he navigates them",
+    ],
+  },
+  {
+    name: "Communication Preferences",
+    content: [
+      "Preferred channels of communication (e.g., email, meetings, video calls)",
+      "Feedback mechanisms",
+      "Communication tone and style (e.g., formal, informal)",
+    ],
+  },
+  {
+    name: "Stakeholder Interactions",
+    content: [
+      "How you engage with different stakeholders (e.g., employees, shareholders, customers)",
+      "Expectations from board meetings, team meetings, etc.",
+    ],
+  },
+  {
+    name: "Industry Insights",
+    content: [
+      "Views on the future of the engineering media industry",
+      "Key competitors and industry benchmarks",
+      "Opportunities and threats in the media landscape",
+    ],
+  },
+  {
+    name: "Crisis Management",
+    content: [
+      "Past experiences managing crises or significant challenges",
+      "Strategies and processes for navigating unforeseen events",
+    ],
+  },
+  {
+    name: "Technology & Innovation",
+    content: [
+      "Views on digital transformation",
+      "Technologies you believe will shape the future",
+      "Innovation strategies",
+    ],
+  },
+];
